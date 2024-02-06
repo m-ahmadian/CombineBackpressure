3739d296bb58f6d7ad5e9d8c245177f6d6670f1d
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-02-05 22:58:31 -0500

    Add Custom Subscriber

--- Backpressure/Code.swift
+++ Backpressure/Code.swift
@@ -23,34 +23,66 @@ func run() {
     
     // MARK: - Throtthle
     
-//    subject
-//        .throttle(
-//            for: 10,
-//            scheduler: DispatchQueue.main,
-//            latest: true
-//        )
-//        .sink { value in
-//            print("new value received: \(value)")
-//        }
-//        .store(in: &cancellables)
+    subject
+        .throttle(
+            for: 10,
+            scheduler: DispatchQueue.main,
+            latest: true
+        )
+        .sink { value in
+            print("new value received: \(value)")
+        }
+        .store(in: &cancellables)
     
     // MARK: - Debounce
     
-//    subject
-//        .debounce(for: 2, scheduler: DispatchQueue.main)
-//        .sink { value in
-//            print("new value received: \(value)")
-//        }
-//        .store(in: &cancellables)
+    subject
+        .debounce(for: 2, scheduler: DispatchQueue.main)
+        .sink { value in
+            print("new value received: \(value)")
+        }
+        .store(in: &cancellables)
     
     // MARK: - 2nd Strategy: Deffer Values
     
     subject
-//        .collect(5)
-//        .collect(.byTime(DispatchQueue.main, 3))
+        .collect(5)
+        .collect(.byTime(DispatchQueue.main, 3))
         .collect(.byTimeOrCount(DispatchQueue.main, 10, 5))
         .sink { value in
             print("new value received: \(value)")
         }
         .store(in: &cancellables)
+    
+    
+    // MARK: - Custom Subscriber
+    
+    class CustomSubscriber: Subscriber {
+        typealias Input = Int
+        typealias Failure = Never
+        
+        var subscription: Subscription?
+        
+        func receive(subscription: Subscription) {
+            print("custom subscriber subscribed")
+            self.subscription = subscription
+            subscription.request(.max(1))
+        }
+        
+        func receive(_ input: Int) -> Subscribers.Demand {
+            print("custom subscriber received: \(input)")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.subscription?.request(.max(2))
+            }
+            
+            return .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Never>) { }
+    }
+
+    subject
+        .subscribe(CustomSubscriber())
+    
 }
